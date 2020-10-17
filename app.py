9acd7690c2bfb3d54fc7650eaf70a49cb23202cb
mahesh sahu <maheshsahu653@outlook.com>
2020-10-17 13:01:34 +0530

    port change

--- app.py
+++ app.py
@@ -98,8 +98,8 @@ def review(product):
         return render_template('results.html')
 
 
-#port = int(os.getenv("PORT"))
+port = int(os.getenv("PORT"))
 if __name__ == "__main__":
     #app.run(host='0.0.0.0', port=5000)
-    #app.run(host='0.0.0.0', port=port)
-    app.run(host='127.0.0.1', port=8001, debug=True)
+    app.run(host='0.0.0.0', port=port)
+    # app.run(host='127.0.0.1', port=8001, debug=True)
